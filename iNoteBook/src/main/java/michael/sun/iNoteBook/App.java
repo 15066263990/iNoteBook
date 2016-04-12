2590dd7df54f452665693ed34f6a241497e5cb2c
Michael.Sun <Michael.Sun@kutesmart.com>
2016-04-12 11:31:55 +0800

    创建工程文件

---
+++ iNoteBook/src/main/java/michael/sun/iNoteBook/App.java
@@ -0,0 +1,13 @@
+package michael.sun.iNoteBook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
